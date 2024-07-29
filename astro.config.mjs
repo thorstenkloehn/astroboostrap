19fc80f77cff42cead24ca72f029edd980066c1e
Thorsten Klöhn <thorstenkloehn@gmail.com>
2024-07-29 10:06:35 +0200

    start

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), react()],
   server: {
     port: 3000
   }
